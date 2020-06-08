68572a8708b0d1917ebbaee224625d4e00f142bd
w135110392 <135110392@qq.com>
2020-06-08 19:10:46 +0800

    增加到 解决 返回的指针 不使用指针解引 也可以输出 但是 是数组越界 还以为是正常的 还是需要用指针接引才对 还有 new 的空间 不能在当前函数体内释放 是因为 中间指向了另外一个地址 中间没有另指 就可以释放

--- students_question/源.cpp
+++ students_question/源.cpp
@@ -1,9 +1,8 @@
+//pdf 格式复制需要先转换成Word格式
 #include<iostream>
 #include<windows.h>
 using namespace std;
-
-
-int main(){
+void for_out(){
 	int a[4][5] = {
 		{ 1 },
 		{ 2 },
@@ -21,11 +20,34 @@ int main(){
 	for (int i = 0; i < 4; i++){
 		for (int j = 0; j< 5; j++){
 
-			cout << a[i %4][j% 5] << " "; //除不了的都是余数 比如 0/4 余0  1/4 余1   
+			cout << a[i % 4][j % 5] << " "; //除不了的都是余数 比如 0/4 余0  1/4 余1   
 		}
 		cout << "\n";
 	}
+}
+void point(){
+	int a[5] = { 1, 10, 3, 4, 6 };
+	int *ptr = (int*)(&a+1); //取地址加1 代表加了一个 同样的数组地址
+	cout << *(a + 1) << endl;
+	cout << *(ptr-1) << endl;  //指针减1 是减一个元素地址 不是一个数组
+}
 
+void dll(){
+	char c[16] = "test";  //new 完后 的过程中 指针另有所指 不能使用delete 释放
+	char *a = new char;
+	a = c;
+	for (int i = 0; i < sizeof(c) / sizeof(char);i++){
+		cout << *(a + i);
+	}
+	cout << "\n";
+	delete a;  //此处不能释放
+
+}
+int main(){
+	
+	dll();
+	cout << "nice to meet you,Chinese.";
 	system("pause");
+	
 	return 0;
 }
